a57d0f50f2b37aaa273662deb02d807418265a9c
q5sys (JT) <jt@obs-sec.com>
2020-07-21 08:07:01 -0400

    Create lsi9300-timeout.m

---
+++ content/en/hub/hardware/fn-certified/lsi9300-timeout.m
@@ -0,0 +1,19 @@
+---
+title: "LSI 9300-xx Firmware Update"
+description: "Updating the LSI 9300-xx Firmware."
+---
+
+## Firmware Information
+
+Servers with the LSI 9300 HBA may experience some performance issues causing the controller to reset when
+using SATA HDDs.
+
+iXsystems has worked with Broadcom to provide a firmware update that resolves the controller reset issue. If
+you have the LSI 9300 HBA with firmware version 16.00.12.00 or earlier installed and would like to resolve the
+occasional controller reset from this issue, follow these instructions. Please note that the firmware update is
+only available from iXsystems.”
+
+Please Note: This problem only applies to firmware versions below 16.00.12.00 and only affects SATA drives.
+SAS drives are not affected.
+
+[LSI 9300-xx Firmware Update Guide]()
