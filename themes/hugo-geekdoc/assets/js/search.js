0a3576b53ce620ce793a9e4ef4500e213f186232
Mrt134 <timmoore88@gmail.com>
2021-03-31 10:36:28 -0400

    test search change

--- themes/hugo-geekdoc/assets/js/search.js
+++ themes/hugo-geekdoc/assets/js/search.js
@@ -14,8 +14,8 @@
   function init() {
     input.removeEventListener('focus', init); // init once
 
-    loadScript('{{ index .Site.Data.assets "js/groupBy.min.js" | relURL }}');
-    loadScript('{{ index .Site.Data.assets "js/flexsearch.min.js" | relURL }}', function() {
+    loadScript('{{ index "js/groupBy.min.js" | relURL }}');
+    loadScript('{{ index "js/flexsearch.min.js" | relURL }}', function() {
       const indexCfg = {{ with .Scratch.Get "geekdocSearchConfig" }}{{ . | jsonify}}{{ else }}{}{{ end }};
       const dataUrl = "{{ $searchData.RelPermalink }}"
 
