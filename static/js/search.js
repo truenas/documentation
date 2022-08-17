7675d417a85a4795867f17dd1836449e14947c30
Tim Moore <Mrt134@users.noreply.github.com>
2022-08-17 16:30:49 -0400

    DOCS-4132: cleaner search implement
    This is a cleaner implementation of a custom search option using lunr.js. It still needs polish, but isn't quite as rough as the previous attempt.

---
+++ static/js/search.js
@@ -0,0 +1,50 @@
+function displayResults (results, store) {
+  const searchResults = document.getElementById('results')
+  if (results.length) {
+    let resultList = ''
+    // Iterate and build result list elements
+    for (const n in results) {
+      const item = store[results[n].ref]
+      resultList += '<li><p><a href="' + item.url + '">' + item.title + '</a></p>'
+      resultList += '<p>' + item.content.substring(0, 150) + '...</p></li>'
+    }
+    searchResults.innerHTML = resultList
+  } else {
+    searchResults.innerHTML = 'No results found.'
+  }
+}
+
+// Get the query parameter(s)
+const params = new URLSearchParams(window.location.search)
+const query = params.get('query')
+
+// Perform a search if there is a query
+if (query) {
+  // Retain the search input in the form when displaying results
+  document.getElementById('search-input').setAttribute('value', query)
+
+  const idx = lunr(function () {
+    this.ref('id')
+    this.field('title', {
+      boost: 15
+    })
+    this.field('tags')
+    this.field('content', {
+      boost: 10
+    })
+
+    for (const key in window.store) {
+      this.add({
+        id: key,
+        title: window.store[key].title,
+        tags: window.store[key].category,
+        content: window.store[key].content
+      })
+    }
+  })
+
+  // Perform the search
+  const results = idx.search(query)
+  // Update the list with results
+  displayResults(results, window.store)
+}
