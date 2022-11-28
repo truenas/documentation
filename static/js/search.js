7b1ee3e3d8980f07ae5cea766f5873908b0e077b
Mrt134 <timmoore88@gmail.com>
2022-11-28 12:53:43 -0500

    DOCS-4132: cleanup
    - Fix Hugo build warning
    - Add bold to .file.dir param returns
    - boost tag search

--- static/js/search.js
+++ static/js/search.js
@@ -6,7 +6,7 @@ function displayResults (results, store) {
     // Iterate and build result list elements
     for (const n in results) {
       const item = store[results[n].ref]
-      resultList += '<li><p>' + item.path + '<a href="' + item.url + '">' + item.title + '</a></p>'
+      resultList += '<li><p><b>' + item.path + '</b>' + '<a href="' + item.url + '">' + item.title + '</a></p>'
       resultList += '<p>' + item.content.substring(0, 150) + '...</p></li>'
     }
     searchResults.innerHTML = resultList
@@ -29,7 +29,9 @@ if (query) {
     this.field('title', {
       boost: 15
     })
-    this.field('tags')
+    this.field('tags', {
+	  boost: 5
+	})
     this.field('content', {
       boost: 10
     })
