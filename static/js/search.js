1812373fcdb63bb8ffd2ca0fc07b369febd51e2b
DjP-iX <133042991+DjP-iX@users.noreply.github.com>
2025-02-06 09:48:53 -0500

    Update search.js

--- static/js/search.js
+++ static/js/search.js
@@ -8,7 +8,7 @@ let loadMoreButton; // Cache reference to the load more button
 initPageFind();
 
 async function initPageFind() {
-    pagefind = await import("https://www.truenas.com/docs/pagefind/pagefind.js");
+    pagefind = await import("https://www.truenas.com/docs/scale/25.04/pagefind/pagefind.js");
     pagefind.init();
 
     if (query != null) {
