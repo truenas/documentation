60d3479df9346269f637fb67c28768f910d5aeac
Dan Pizappi <133042991+DjP-iX@users.noreply.github.com>
2025-09-09 13:59:11 -0400

    PD-2171 / 26.04 / Pd 2171 the capacity calculator isnt working (#4104)
    * Update capacity_calculator.html
    
    * Update capacity.js
    
    * Update linkable-tabs-init.js
    
    * Update linkable-tabs.js
    
    * Update capacity_calculator.html
    
    * Update capacity.js
    
    * Update capacity_calculator.html
    
    * Update capacity.js
    
    * Update capacity_calculator.html
    
    * Update capacity.js
    
    * Update capacity.js
    
    * Update capacity_calculator.html
    
    * Update capacity_calculator.html
    
    * Update capacity_calculator.html
    
    * Update capacity.js
    
    * Update capacity_calculator.html
    
    * Update capacity.js
    
    * Update ZFSCapacityCalculator.md
    
    * Update capacity_calculator.html
    
    * simple color fix
    
    ---------
    
    Co-authored-by: Tim Moore <Mrt134@users.noreply.github.com>

--- static/js/linkable-tabs-init.js
+++ static/js/linkable-tabs-init.js
@@ -4,8 +4,9 @@
  * @param {string} sourceId - ID of the source container with tab content
  * @param {string} containerId - ID of the target container for tabs  
  * @param {string} defaultTab - Optional default tab ID
+ * @param {boolean} autoOpen - Whether to auto-open default tab (defaults to false)
  */
-function initializeHugoTabs(sourceId, containerId, defaultTab = null) {
+function initializeHugoTabs(sourceId, containerId, defaultTab = null, autoOpen = false) {
     // Extract all tab content from Hugo-processed divs
     const tabSource = document.getElementById(sourceId);
     const tabDivs = tabSource.querySelectorAll('[data-tab-id]');
@@ -19,7 +20,8 @@ function initializeHugoTabs(sourceId, containerId, defaultTab = null) {
     const options = {
         urlHashEnabled: true,
         enableMarkdown: false, // Hugo already processed the content
-        collapsible: true
+        collapsible: true,
+        autoOpen: autoOpen // Pass the autoOpen parameter
     };
 
     if (defaultTab) {
